f7a3f980fc0448a3fbdb35fd9055c169cb6885de
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-03 15:57:31 +0100

    Sprint 1 began
    Added a player and added a remove platforms method

---
+++ AINT 254 project/Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour
+{
+    public List<GameObject> blocks = new List<GameObject>();
+    void Update()
+    {
+        PickUpBlocks();
+
+
+    }
+
+    void PickUpBlocks()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 500.0f))
+            {
+                Debug.Log("you hit " + hit.transform.name);
+                blocks.Add(hit.collider.gameObject);
+
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+
+
+    void PlaceBlocks()
+    {
+
+    }
+
+}
---
+++ AINT 254 project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            rb.velocity = -transform.forward * speed;
+        }
+    }
+
+
+
+
+}
