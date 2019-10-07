c2106cbfad28b70006925f4525184a6ef25bb243
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-07 14:00:40 +0100

    Working on block placement script

--- AINT 254 project/Assets/Scripts/BlockMovement.cs
+++ AINT 254 project/Assets/Scripts/BlockMovement.cs
@@ -1,37 +1,68 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockMovement : MonoBehaviour
 {
-    public List<GameObject> blocks = new List<GameObject>();
+    public GameObject block;
+    public Queue<GameObject> blocks = new Queue<GameObject>();
+    RaycastHit blockHit;
+
+
+
     void Update()
     {
-        PickUpBlocks();
+        if (Input.GetMouseButtonDown(0))
+        {
+            PickUpBlocks();
+        }
 
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlaceBlocks();
+        }
 
     }
 
     void PickUpBlocks()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 500.0f))
-            {
-                Debug.Log("you hit " + hit.transform.name);
-                blocks.Add(hit.collider.gameObject);
 
-                Destroy(hit.collider.gameObject);
-            }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out blockHit, 500.0f))
+        {
+
+
+            Destroy(blockHit.collider.gameObject); // Destroy the block from the world and instiatiate a new block ready to be placed
+
+            blocks.Enqueue(Instantiate(block));
+
+
         }
+
     }
 
 
     void PlaceBlocks()
     {
+        GameObject blockToPlace;
+
+        
+        blockToPlace = blocks.Dequeue();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out blockHit, 500.0f))
+        {
+            blockHit.collider.gameObject.SetActive(true);
+            //Generate new block
+            Vector3 blockPos = blockHit.point + blockHit.normal / 2.0f;
+
+            blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero);
+            blockPos.y = (float)Math.Round(blockPos.y, MidpointRounding.AwayFromZero);
+            blockPos.z = (float)Math.Round(blockPos.z, MidpointRounding.AwayFromZero);
+
+            GameObject block = (GameObject)Instantiate(blockToPlace, blockPos, Quaternion.identity);
+
+        }
 
     }
-
 }
