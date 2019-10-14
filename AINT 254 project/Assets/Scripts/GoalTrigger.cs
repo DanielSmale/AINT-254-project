5d5b25263e1e3e392f780cfe4d17d797683fcb7b
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-14 16:02:06 +0100

    Added goal block

---
+++ AINT 254 project/Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("You win");
+    }
+
+
+}
