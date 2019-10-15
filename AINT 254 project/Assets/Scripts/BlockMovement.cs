bc53e858f14083d3902c146d2df1e33661f229b0
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-15 15:30:24 +0100

    Fixing block placement but still have queue empty error

--- AINT 254 project/Assets/Scripts/BlockMovement.cs
+++ AINT 254 project/Assets/Scripts/BlockMovement.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BlockMovement : MonoBehaviour
 {
+    //Code referenced: https://www.youtube.com/watch?v=4pvuBl_PQKs&list=PLi-ukGVOag_1Ux4oXpm8CA_UH_VDl5tTV&index=2&t=0s
+
     public GameObject block;
     public Queue<GameObject> blocks = new Queue<GameObject>();
     RaycastHit blockHit;
+
+    // Bit shift the index of the layer (8) to get a bit mask
+    int layerMask = 1 << 8;
+
 
 
 
@@ -28,9 +34,8 @@ public class BlockMovement : MonoBehaviour
     {
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out blockHit, 500.0f))
+        if (Physics.Raycast(ray, out blockHit, 500.0f, layerMask))
         {
-
 
             Destroy(blockHit.collider.gameObject); // Destroy the block from the world and instiatiate a new block ready to be placed
 
@@ -45,18 +50,17 @@ public class BlockMovement : MonoBehaviour
     void PlaceBlocks()
     {
         GameObject blockToPlace;
-
         
+
         blockToPlace = blocks.Dequeue();
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out blockHit, 500.0f))
+        if (Physics.Raycast(ray, out blockHit, 500.0f, layerMask) && blockHit.collider.gameObject.name == "PlaceableArea")
         {
-            blockHit.collider.gameObject.SetActive(true);
             //Generate new block
-            Vector3 blockPos = blockHit.point + blockHit.normal / 2.0f;
+            Vector3 blockPos = blockHit.point;
 
-            blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero);
+            blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero); // Round the blocks position to the nearest whole number placing it in a more uniform fashion
             blockPos.y = (float)Math.Round(blockPos.y, MidpointRounding.AwayFromZero);
             blockPos.z = (float)Math.Round(blockPos.z, MidpointRounding.AwayFromZero);
 
@@ -66,3 +70,6 @@ public class BlockMovement : MonoBehaviour
 
     }
 }
+
+
+        
