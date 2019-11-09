d9a8600a29e5165c2615f3d6ec2702fef2d45fed
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-11-09 16:43:23 +0000

    Falling block code began
    May need to look at making an abstract block class that i construct blocks from

---
+++ AINT 254 project/Assets/FallingBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Rigidbody blockRB;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        blockRB.useGravity = true;
+    }
+
+
+
+}
